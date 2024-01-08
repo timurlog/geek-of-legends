6a68a54c5c82eeee69f06f9cd8b8aba54e7840ab
Adonan17 <adnaneouassari@gmail.com>
2024-01-08 14:05:14 +0100

    adding some instances and variables

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+// importing what needs to be imported
+import {Hero} from './class.js';
+
+// creating variables for all buttons
+
+// menu buttons
+let startGameBtn = document.querySelector('#startGameBtn');
+let lilithChoice = document.querySelector('#lilithChoice');
+let sauronChoice = document.querySelector('#sauronChoice');
+let chronosChoice = document.querySelector('#chronosChoice');
+
+
+
+// creating hero objects with Hero class
+let warrior = new Hero();
+let mage = new Hero();
+let archer = new Hero();
