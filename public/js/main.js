dd20fdf66b45972f7c456064470f1f9adfdc9c74
Adonan17 <adnaneouassari@gmail.com>
2024-01-08 16:59:42 +0100

    adding menu navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,46 @@
 // importing what needs to be imported
-import {Hero, Boss, Warrior, Mage, Archer} from './class.js';
+import {Boss, Warrior, Mage, Archer} from './class.js';
 
 // menu buttons
-export let startGameBtn = document.querySelector('#startGameBtn');
-export let replayGameBtn = document.querySelector('#replayGameBtn')
-export let lilithBtn = document.querySelector('#lilithBtn');
-export let sauronBtn = document.querySelector('#sauronBtn');
-export let chronosBtn = document.querySelector('#chronosBtn');
-export let confirmStatsBtn = document.querySelector('#confirmStatsBtn');
+let mainMenu = document.querySelector('#mainMenu')
+let mainMenuBoss = document.querySelector('#mainMenuBoss')
+let mainMenuHero = document.querySelector('#mainMenuHero')
+let gameplay = document.querySelector('#gameplay')
+let restartMenu = document.querySelector('#restartMenu')
+let startGameBtn = document.querySelector('#startGameBtn');
+let restartGameBtn = document.querySelector('#replayGameBtn')
+let lilithBtn = document.querySelector('#lilithBtn');
+let sauronBtn = document.querySelector('#sauronBtn');
+let chronosBtn = document.querySelector('#chronosBtn');
+let confirmStatsBtn = document.querySelector('#confirmStatsBtn');
 
 // character maker inputs
-export let warriorNameInput = document.querySelector('#warriorNameInput')
-export let mageNameInput = document.querySelector('#mageNameInput')
-export let archerNameInput = document.querySelector('#archerNameInput')
-export let warriorHealthPointsInput = document.querySelector('#warriorHealthPointsBtn')
-export let mageHealthPointsInput = document.querySelector('#mageHealthPointsBtn')
-export let archerHealthPointsInput = document.querySelector('#archerHealthPointsBtn')
-export let warriorDamagePointsInput = document.querySelector('#warriorDamagePointsInput')
-export let mageDamagePointsInput = document.querySelector('#mageDamagePointsInput')
-export let archerDamagePointsInput = document.querySelector('#archerDamagePointsInput')
-export let warriorAttackModeMenuBtn = document.querySelector('#warriorAttackModeMenuBtn')
-export let mageAttackModeMenuBtn = document.querySelector('#mageAttackModeMenuBtn')
-export let archerAttackModeMenuBtn = document.querySelector('#archerAttackModeMenuBtn')
-export let warriorDefenseModeMenuBtn = document.querySelector('#warriorDefenseModeMenuBtn')
-export let mageDefenseModeMenuBtn = document.querySelector('#mageDefenseModeMenuBtn')
-export let archerDefenseModeMenuBtn = document.querySelector('#archerDefenseModeMenuBtn')
+let warriorNameInput = document.querySelector('#warriorNameInput')
+let mageNameInput = document.querySelector('#mageNameInput')
+let archerNameInput = document.querySelector('#archerNameInput')
+let warriorHealthPointsInput = document.querySelector('#warriorHealthPointsBtn')
+let mageHealthPointsInput = document.querySelector('#mageHealthPointsBtn')
+let archerHealthPointsInput = document.querySelector('#archerHealthPointsBtn')
+let warriorDamagePointsInput = document.querySelector('#warriorDamagePointsInput')
+let mageDamagePointsInput = document.querySelector('#mageDamagePointsInput')
+let archerDamagePointsInput = document.querySelector('#archerDamagePointsInput')
+let warriorAttackModeMenuBtn = document.querySelector('#warriorAttackModeMenuBtn')
+let mageAttackModeMenuBtn = document.querySelector('#mageAttackModeMenuBtn')
+let archerAttackModeMenuBtn = document.querySelector('#archerAttackModeMenuBtn')
+let warriorDefenseModeMenuBtn = document.querySelector('#warriorDefenseModeMenuBtn')
+let mageDefenseModeMenuBtn = document.querySelector('#mageDefenseModeMenuBtn')
+let archerDefenseModeMenuBtn = document.querySelector('#archerDefenseModeMenuBtn')
 
 // character maker variables
-export let warriorName = warriorNameInput.innerHTML
-export let mageName = mageNameInput.innerHTML
-export let archerName = archerNameInput.innerHTML
-export let warriorHealth = warriorHealthPointsInput.innerHTML
-export let mageHealth = mageHealthPointsInput.innerHTML
-export let archerHealth = archerHealthPointsInput.innerHTML
-export let warriorDamage = warriorDamagePointsInput.innerHTML
-export let mageDamage = mageDamagePointsInput.innerHTML
-export let archerDamage = archerDamagePointsInput.innerHTML
+let warriorName = warriorNameInput.innerHTML
+let mageName = mageNameInput.innerHTML
+let archerName = archerNameInput.innerHTML
+let warriorHealth = warriorHealthPointsInput.innerHTML
+let mageHealth = mageHealthPointsInput.innerHTML
+let archerHealth = archerHealthPointsInput.innerHTML
+let warriorDamage = warriorDamagePointsInput.innerHTML
+let mageDamage = mageDamagePointsInput.innerHTML
+let archerDamage = archerDamagePointsInput.innerHTML
 
 // event listeners for the combat mode in the menu
 warriorAttackModeMenuBtn.addEventListener('click', () => {
@@ -63,16 +68,65 @@ archerDefenseModeMenuBtn.addEventListener('click', () => {
 })
 
 // adding hero special stats
-export let rage = 0
-export let mana = 7
-export let arrows = 6
+let rage = 0
+let mana = 7
+let arrows = 6
 
 // creating hero objects with Hero class
-export let warrior = new Warrior(warriorName, warrior.combatMode, warriorDamage, warriorHealth, rage);
-export let mage = new Mage(mageName, mage.combatMode, mageDamage, mageHealth, mana);
-export let archer = new Archer(archerName, archer.combatMode, archerDamage, archerHealth, arrows);
+let warrior = new Warrior(warriorName, warrior.combatMode, warriorDamage, warriorHealth, rage);
+let mage = new Mage(mageName, mage.combatMode, mageDamage, mageHealth, mana);
+let archer = new Archer(archerName, archer.combatMode, archerDamage, archerHealth, arrows);
 
 // creating boss objects with Boss class
-export let lilith = new Boss('Lilith', 30, 900)
-export let chronos = new Boss('Chronos', 30, 1000)
-export let sauron = new Boss('Sauron', 25, 1150)
+let lilith = new Boss('Lilith', 30, 900)
+let chronos = new Boss('Chronos', 30, 1000)
+let sauron = new Boss('Sauron', 25, 1150)
+
+// handling menu through event listeners
+startGameBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'none'
+    mainMenuBoss.style.display = 'block'
+    mainMenuHero.style.display = 'none'
+    gameplay.style.display = 'none'
+    restartMenu.style.display = 'none'
+})
+
+lilithBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'none'
+    mainMenuBoss.style.display = 'none'
+    mainMenuHero.style.display = 'block'
+    gameplay.style.display = 'none'
+    restartMenu.style.display = 'none'
+})
+
+sauronBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'none'
+    mainMenuBoss.style.display = 'none'
+    mainMenuHero.style.display = 'block'
+    gameplay.style.display = 'none'
+    restartMenu.style.display = 'none'
+})
+
+chronosBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'none'
+    mainMenuBoss.style.display = 'none'
+    mainMenuHero.style.display = 'block'
+    gameplay.style.display = 'none'
+    restartMenu.style.display = 'none'
+})
+
+confirmStatsBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'none'
+    mainMenuBoss.style.display = 'none'
+    mainMenuHero.style.display = 'none'
+    gameplay.style.display = 'block'
+    restartMenu.style.display = 'none'
+})
+
+restartGameBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'none'
+    mainMenuBoss.style.display = 'block'
+    mainMenuHero.style.display = 'none'
+    gameplay.style.display = 'none'
+    restartMenu.style.display = 'none'
+})
