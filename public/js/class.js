5bcba3f8fac9e58371b96abdd0462c1d35a105b4
Adonan17 <adnaneouassari@gmail.com>
2024-01-08 11:37:57 +0100

    creating hero class

---
+++ public/js/class.js
@@ -0,0 +1,8 @@
+export class Hero {
+    constructor(name, combatMode, damage, health){
+        this.name = name,
+        this.combatMode = combatMode,
+        this.damage = damage,
+        this.health = health
+    }
+}
