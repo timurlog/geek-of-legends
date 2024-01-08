283f88aa0be063206e3bddae1143c29ca92a536e
Adonan17 <adnaneouassari@gmail.com>
2024-01-08 14:53:35 +0100

    creating all hero classes

--- public/js/class.js
+++ public/js/class.js
@@ -13,5 +13,22 @@ export class Hero {
 
 // creating the different heroes classes
 export class Warrior extends Hero {
-    constructor(name, combatMode, damage, health, rage)
+    constructor(name, combatMode, damage, health, rage){
+        super(name, combatMode, damage, health)
+        this.rage = rage
+    }
+}
+
+export class Mage extends Hero {
+    constructor(name, combatMode, damage, health, mana){
+        super(name, combatMode, damage, health)
+        this.mana = mana
+    }
+}
+
+export class Archer extends Hero {
+    constructor(name, combatmode, damage, health, arrows){
+        super(name, combatmode, damage, health)
+        this.arrows = arrows
+    }
 }
